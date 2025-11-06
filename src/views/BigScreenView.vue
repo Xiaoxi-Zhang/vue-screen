8f58b093d8e4c34c081447d3a94019ccd03acdf3
Xiaoxi-Zhang <96587298+Xiaoxi-Zhang@users.noreply.github.com>
2025-11-06 18:18:43 +0800

    2D图表渲染

--- src/views/BigScreenView.vue
+++ src/views/BigScreenView.vue
@@ -1,10 +1,10 @@
 <script setup>
 import { getParkInfoAPI } from '@/api/park'
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 import * as echarts from 'echarts'
 // 1.导入echarts包
 // 2.获取要渲染的dom元素（准备好了宽高、并且已经渲染到页面中）
-// 3.把dom元素传入到echart.init()  获取echarts实例
+// 3.把dom元素传入到echarts.init()  获取echarts实例
 // 4.准备渲染echarts的配置项
 // 5.把配置项 传给echarts实例
 
@@ -16,7 +16,121 @@ const getParkInfo = async () => {
   // console.log(res)
   parkInfo.value = res.data
 }
-getParkInfo()
+
+// 渲染柱状图
+const barChart = ref(null)
+const initBarChart = () => {
+  const parkIncome = parkInfo.value.parkIncome
+  const myChart = echarts.init(barChart.value)
+  const option = {
+    tooltip: {
+      trigger: 'axis',
+      axisPointer: {
+        type: 'shadow',
+      },
+    },
+    grid: {
+      // 让图表占满容器
+      top: '10px',
+      left: '0px',
+      right: '0px',
+      bottom: '0px',
+      containLabel: true,
+    },
+    xAxis: [
+      {
+        type: 'category',
+        axisTick: {
+          alignWithLabel: true,
+          show: false,
+        },
+        data: parkIncome.xMonth,
+      },
+    ],
+    yAxis: [
+      {
+        type: 'value',
+        splitLine: {
+          show: false,
+        },
+      },
+    ],
+    series: [
+      {
+        name: '园区年度收入',
+        type: 'bar',
+        barWidth: '10px',
+        data: parkIncome.yIncome.map((item, index) => {
+          const color =
+            index % 2 === 0
+              ? new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+                { offset: 0.23, color: '#74c0f8' },
+                { offset: 1, color: 'rgba(116,192,248,0.00)' },
+              ])
+              : new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+                { offset: 0.23, color: '#ff7152' },
+                { offset: 1, color: 'rgba(255,113,82,0.00)' },
+              ])
+          return { value: item, itemStyle: { color } }
+        }),
+      },
+    ],
+    textStyle: {
+      color: '#B4C0CC',
+    },
+  }
+  myChart.setOption(option)
+}
+
+// 渲染饼状图
+const pieChart = ref(null)
+const initPieChart = () => {
+  const myPieChart = echarts.init(pieChart.value)
+  const parkIndustry = parkInfo.value.parkIndustry
+  const pieOption = {
+    color: [
+      '#00B2FF', '#2CF2FF', '#892CFF', '#FF624D', '#FFCF54', '#86ECA2'],
+    legend: {
+      itemGap: 20,
+      bottom: '0',
+      icon: 'rect',
+      itemHeight: 10, // 图例icon高度
+      itemWidth: 10, // 图例icon宽度
+      textStyle: {
+        color: '#c6d1db',
+      },
+    },
+    tooltip: {
+      trigger: 'item'
+    },
+    series: [
+      {
+        name: '园区产业分析',
+        type: 'pie',
+        radius: ['55%', '60%'], // 设置内圈与外圈的半径使其呈现为环形
+        center: ['50%', '40%'], // 圆心位置， 用于调整整个图的位置
+        tooltip: {
+          trigger: 'item',
+          formatter: (params) => {
+            return `${params.seriesName}</br><div style='display:flex;justify-content: space-between;'><div>${params.marker}${params.name}</div><div>${params.percent}%</div></div>`
+          }
+        },
+        label: {
+          show: false,
+          position: 'center',
+        },
+        data: parkIndustry,
+      },
+    ],
+  }
+  myPieChart.setOption(pieOption)
+}
+
+onMounted(async () => {
+  await getParkInfo()
+  initBarChart()
+  initPieChart()
+})
 
 </script>
 
@@ -32,6 +146,8 @@ getParkInfo()
         <div class="item">
           <div class="icons-item building-icon">
             <span class="number">
+              <!-- 可选链 -->
+              <!-- {{ parkInfo.base?.buildingTotal }} -->
               {{ parkInfo.base.buildingTotal }}
             </span>
           </div>
@@ -81,6 +197,13 @@ getParkInfo()
         </div>
       </div>
       <div class="bar-chart" ref="barChart"></div>
+    </div>
+    <!-- 园区产业分布 -->
+    <div class="section-three">
+      <img class="img-header"
+        src="https://yjy-teach-oss.oss-cn-beijing.aliyuncs.com/smartPark/%E5%A4%A7%E5%B1%8F%E5%88%87%E5%9B%BE/%E5%9B%AD%E5%8C%BA%E4%BA%A7%E4%B8%9A%E5%88%86%E5%B8%83%402x.png"
+        alt="" />
+      <div class="pie-chart" ref="pieChart"></div>
     </div>
   </div>
 </template>
@@ -167,6 +290,18 @@ getParkInfo()
   .bar-chart {
     width: 100%;
     height: calc(100% - 90px);
+  }
+}
+
+.section-three {
+  flex-basis: 40%;
+
+  .pie-chart {
+    position: relative;
+    margin: 0 auto;
+    padding-bottom: 20px;
+    width: 80%;
+    height: calc(100% - 40px);
   }
 }
 </style>
